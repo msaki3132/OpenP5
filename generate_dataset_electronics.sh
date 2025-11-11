e5da14238d8b017f01ef6c30f55bfb0a89178285
masaki rikitoku <msaki3132@rikima.com>
2025-11-12 00:38:00 +0900

    add

---
+++ generate_dataset_electronics.sh
@@ -0,0 +1,15 @@
+ts=100
+cluster=10
+for dataset in Electronics
+do
+    for indexing in random sequential collaborative
+    do
+        python ./src/src_llama/generate_dataset.py --dataset ${dataset} --data_path ./data/ --item_indexing ${indexing} --tasks sequential,straightforward --prompt_file ./prompt.txt --collaborative_token_size ${ts} --collaborative_cluster ${cluster}
+
+        python ./src/src_llama/generate_dataset_eval.py --dataset ${dataset} --data_path ./data/ --item_indexing ${indexing} --tasks sequential,straightforward --prompt_file ./prompt.txt --mode validation --prompt seen:0 --collaborative_token_size ${ts} --collaborative_cluster ${cluster}
+
+        python ./src/src_llama/generate_dataset_eval.py --dataset ${dataset} --data_path ./data/ --item_indexing ${indexing} --tasks sequential,straightforward --prompt_file ./prompt.txt --mode test --prompt seen:0 --collaborative_token_size ${ts} --collaborative_cluster ${cluster}
+
+        python ./src/src_llama/generate_dataset_eval.py --dataset ${dataset} --data_path ./data/ --item_indexing ${indexing} --tasks sequential,straightforward --prompt_file ./prompt.txt --mode test --prompt unseen:0 --collaborative_token_size ${ts} --collaborative_cluster ${cluster}
+    done
+done
